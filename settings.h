98e0a2a99efb30b0bdff753a0b361258337a6063
richardvdweerd <richard.vd.weerd@gmail.com>
2017-04-11 16:02:53 +0200

    Create settings.h
    Settings file to store personalized settings.

---
+++ settings.h
@@ -0,0 +1,11 @@
+#define SSID  "<SSID>"
+#define PASSWORD "<SSID password>"
+#define CONTYPE "http"
+#define DOMUSER "<domoticzuser>"
+#define DOMPASS "<domoticzuserpassword>"
+#define DOMIP "<ipaddress of domoticz-server>"
+// Domoticz port number:
+#define DOMPORT 8080  
+// Domoticz index number of device:
+#define PIRIDX 160
+#define ESPNAME "<Name of Pir, eg: espPirKitchen>"
